0c61516e6845ef5b3c84818a2e655439f48e1623
Caedmarroquin <99516348+Caedmarroquin@users.noreply.github.com>
2022-09-01 23:23:31 -0600

    Add files via upload

---
+++ CalculodeFactorial-ModoConsola/CalculodeFactorial-ModoConsola/Module1.vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+    Sub Main()
+        Dim acum, i, num, suma As Integer
+        acum = 1
+        Console.WriteLine("Ingrese Número:")
+        num = Console.ReadLine()
+        If (num > 0) Then
+            For i = 1 To num
+                acum = acum * i
+            Next
+        End If
+        Console.WriteLine("Factorial: " & acum)
+        Console.WriteLine("")
+        Console.WriteLine("Multiplos de " & num)
+        For indice As Integer = 0 To acum Step 4
+            Console.WriteLine(indice)
+            suma = suma + 1
+        Next
+        Console.WriteLine("Hay " & suma & " multiplos de " & num)
+        Console.WriteLine("")
+        Console.WriteLine("Pulse cualquier tecla para salir")
+        Console.ReadKey()
+    End Sub
+    
+End Module
